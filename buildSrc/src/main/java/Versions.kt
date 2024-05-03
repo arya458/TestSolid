a854ab539238ab547dd083393703269440ddd394
Arya Danesh <arya717375@Gmail.com>
2024-05-03 08:25:59 +0430

    Some Dependencies Added And Min SDK set To 24 Because of Min Api Support of Our Dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -11,5 +11,13 @@ object Versions {
     const val activityCompose = "1.9.0"
     const val composeBom = "2023.08.00"
     const val jetbrainsKotlinJvm = "1.9.0"
+    const val hiltAndroid = "2.48"
+    const val hiltLifecycleViewmodel = "1.2.0"
+    const val hiltNavigationCompose = "1.2.0"
+    const val play_services_auth= "20.5.0"
+    const val play_services_auth_api_phone="18.0.1"
+    const val work_version="2.9.0"
+
+
 
 }
